d48f67bc804ed1e77a3ea6b6fe6f7ef7dcf79243
liliacharfi <charfi.lilia2@gmail.com>
2026-02-27 00:28:22 +0100

    Login / docProfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@
     import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
     import Home from '../pages/Home'
     import Register from '../pages/Register'
-    //import Login from '../pages/Login'
+    import Login from '../pages/Login'
+    import DoctorProfile from '../pages/DoctorProfile'
 
 
     function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/drprofile" element={<DoctorProfile/>} />
+
       </Routes>
   )
 }
